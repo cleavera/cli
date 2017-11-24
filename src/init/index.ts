adbc500860f9eaf9420e68776eba40c5f435ebb9
Anthony Cleaver <anthony.cleaver@mhr.co.uk>
2017-11-24 11:37:40 +0000

    do npm install

--- src/init/index.ts
+++ src/init/index.ts
@@ -5,6 +5,16 @@ import * as template from './resources';
 export async function run(input: Input): Promise<void> {
     const hasTemplate: boolean = await input.bool('Does this project have a template?');
     let isAngular: boolean = false;
+    const devDep: Array<string> = [
+        'typescript',
+        'tslint',
+        'webpack',
+        'ts-loader',
+        'raw-loader',
+        'sass-loader',
+        'node-sass'
+    ];
+    const dep: Array<string> = [];
 
     if (hasTemplate) {
         isAngular = await input.bool('Is this project an angular project?');
@@ -13,7 +23,20 @@ export async function run(input: Input): Promise<void> {
     let webpack: string;
 
     if (hasTemplate) {
+        devDep.push('html-webpack-plugin');
+
         if (isAngular) {
+            devDep.push('angular2-template-loader');
+            dep.push('@angular/common');
+            dep.push('@angular/compiler');
+            dep.push('@angular/core');
+            dep.push('@angular/platform-browser');
+            dep.push('@angular/platform-browser-dynamic');
+            dep.push('core-js');
+            dep.push('reflect-metadata');
+            dep.push('rxjs');
+            dep.push('zone.js');
+
             webpack = template.webpackAngular;
         } else {
             webpack = template.webpackTemplate;
@@ -28,4 +51,9 @@ export async function run(input: Input): Promise<void> {
     fs.writeFileSync('./tslint.json', template.tslint);
     fs.writeFileSync('./tsconfig.json', template.tsconfig);
     fs.writeFileSync('./webpack.config.js', webpack);
+
+    await child(`npm.cmd i ${devDep.join(' ')} --save-dev`);
+    if (dep.length) {
+        await child(`npm.cmd i ${dep.join(' ')} --save`);
+    }
 }
