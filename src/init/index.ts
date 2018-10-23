61742244c8e77c49a6cb0890d7390d71d5e1af85
Anthony Cleaver <anthony.cleaver@mhr.co.uk>
2018-10-23 10:15:31 +0100

    Tweaks

--- src/init/index.ts
+++ src/init/index.ts
@@ -99,7 +99,7 @@ export async function run(input: Input): Promise<void> {
         fs.writeFileSync('./.npmignore', template.npmIgnore);
 
         scripts.lint = `tslint --project ./tsconfig.json`;
-        scripts.prepublish = `npm run lint && npm run build`;
+        scripts.prepack = `npm run lint && npm run build`;
     }
 
     if (isLibrary) {
